3822ad96758132d3c9f6fd5c328525dc7039b1f8
Christian Ferraz <christianferraz@gmail.com>
2022-04-05 02:20:33 -0400

    Alter file TaskList.tsx Christian Ferraz v2

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,7 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     let idTemp = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1
-    setTasks([...tasks, { id: idTemp, title: newTaskTitle, isComplete: false }])
+    setTasks(oldTask => [...oldTask, { id: idTemp, title: newTaskTitle, isComplete: false }])
     setNewTaskTitle('')
   }
 
